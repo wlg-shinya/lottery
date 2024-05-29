d03511c24cd4351271285101df176baefe1c5926
s-watanabe <s-watanabe@three-rings.net>
2024-05-29 12:45:05 +0900

    refactor

--- src/components/Lottery.vue
+++ src/components/Lottery.vue
@@ -5,40 +5,39 @@ import FlexTextarea from "./FlexTextarea.vue";
 
 const PLACEHOLDER_TEXT = "一行がひとつのくじとなります\n空白行は無視されます";
 
-const inputLotteryList = ref("");
-const resultLottery = ref("");
+const input = ref("");
+const result = ref("");
 
 // TODO:認証情報をもとに編集可能かどうかを判断するように。DB書き込みも同様にする必要がある
 const editable = ref(true);
 
 // 入力された文字に変化あり次第ローカルストレージに保存
-watch([inputLotteryList, resultLottery], () =>
+watch([input, result], () =>
   LocalStorageLottery.save({
-    input: inputLotteryList.value,
-    result: resultLottery.value,
+    input: input.value,
+    result: result.value,
   })
 );
 
-// 抽選文字列一覧
-// - 改行単位
-// - 空行は抽選対象外
-const lotteryList = computed(() => inputLotteryList.value.split("\n").filter((x) => x));
+// 抽選対象群
+// PLACEHOLDER_TEXT の条件をここで表現
+const lotteryTargets = computed(() => input.value.split("\n").filter((x) => x));
 
 async function onStart() {
   await LocalStorageLottery.setup();
   // ローカルストレージのデータから初期文字列を復元
   await LocalStorageLottery.load().then((data) => {
-    inputLotteryList.value = data.input;
-    resultLottery.value = data.result;
+    input.value = data.input;
+    result.value = data.result;
   });
 }
 
 function onClickLotteryButton() {
-  resultLottery.value = lotteryList.value[random(lotteryList.value.length)];
+  result.value = lotteryTargets.value[random(lotteryTargets.value.length)];
 }
 
 function onInputFlexTextarea(text: string) {
-  inputLotteryList.value = text;
+  input.value = text;
 }
 
 function random(max: number) {
@@ -51,19 +50,13 @@ onStart();
 
 <template>
   <div class="d-flex flex-column align-items-center">
-    <FlexTextarea
-      @input="onInputFlexTextarea"
-      :initText="inputLotteryList"
-      :placeholder="PLACEHOLDER_TEXT"
-      :disabled="!editable"
-      style="min-width: 250px"
-    />
+    <FlexTextarea @input="onInputFlexTextarea" :initText="input" :placeholder="PLACEHOLDER_TEXT" :disabled="!editable" style="min-width: 250px" />
     <div>
       <button @click="onClickLotteryButton()" class="btn btn-primary btn-lg">抽選</button>
     </div>
-    <div v-if="resultLottery">
+    <div v-if="result">
       <span>結果</span>
-      <h1>{{ resultLottery }}</h1>
+      <h1>{{ result }}</h1>
     </div>
   </div>
 </template>
