4daec4d17b395609dc2dcd2a2ab5ade52a53dd03
s-watanabe <s-watanabe@three-rings.net>
2024-06-03 14:28:22 +0900

    くじ引きデータ作成API(post)の用意

--- srv/api/routers/lotteries.py
+++ srv/api/routers/lotteries.py
@@ -7,3 +7,7 @@ router = APIRouter()
 @router.get("/api/read_lotteries", response_model=List[schema.Lotteries])
 async def read_lotteries():
     return [schema.Lotteries(id=1, user_id=1, text="FOO")]
+
+@router.post("/api/create_lottery", response_model=schema.LotteryCreateResponse)
+async def create_lottery(body: schema.LotteryCreate):
+    return schema.LotteryCreateResponse(id=1, **body.model_dump())
--- srv/api/schemas/lotteries.py
+++ srv/api/schemas/lotteries.py
@@ -1,8 +1,17 @@
 from typing import Optional
 from pydantic import BaseModel, Field
 
-class Lotteries(BaseModel):
-    id: int
-    user_id: int = Field(desciption="このくじ引きの所有者ID")
+class LotteriesBase(BaseModel):
+    user_id: int = Field(-1, desciption="このくじ引きの所有者ID")
     text: str = Field("", desciption="くじ引きの抽選対象")
     title: str = Field("", desciption="くじ引き名")
+    pass
+
+class LotteryCreate(LotteriesBase):
+    pass
+
+class LotteryCreateResponse(LotteryCreate):
+    id: int
+
+class Lotteries(LotteriesBase):
+    id: int
