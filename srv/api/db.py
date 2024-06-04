68bb8c1228bf3841afd3b2d6abe05e70872059a6
s-watanabe <s-watanabe@three-rings.net>
2024-06-04 10:31:59 +0900

    不要な定義を削除

--- srv/api/db.py
+++ srv/api/db.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
 from core.config import env
 
 engine = create_async_engine(env().get_async_db_url(), echo=True)
@@ -9,8 +9,6 @@ async_session = sessionmaker(
     bind = engine,
     class_=AsyncSession
 )
-
-Base = declarative_base()
 
 async def db():
     async with async_session() as session:
