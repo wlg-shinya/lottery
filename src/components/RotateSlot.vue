f39648653539180d9c8154f7b4da9cb1eaa17311
s-watanabe <s-watanabe@three-rings.net>
2024-06-06 10:05:49 +0900

    スロット回転表現の実装

--- src/components/RotateSlot.vue
+++ src/components/RotateSlot.vue
@@ -1,12 +1,30 @@
 <script setup lang="ts">
+// スロット回転表現
+// あくまでも表現でありこのコンポネントから回転中の値を取得することは想定していない（≒目押し禁止）
+import { ref } from "vue";
+
 const props = defineProps<{
   slots: string[];
   slotClass: string;
 }>();
+
+const slotIndex = ref(0);
+
+function incrementSlotIndex() {
+  if (slotIndex.value < props.slots.length - 1) slotIndex.value++;
+  else slotIndex.value = 0;
+}
+
+function update() {
+  incrementSlotIndex();
+  setTimeout(() => update(), 64);
+}
+
+update();
 </script>
 
 <template>
   <div>
-    <div :class="slotClass">{{ slots[0] }}</div>
+    <div :class="slotClass">{{ slots[slotIndex] }}</div>
   </div>
 </template>
