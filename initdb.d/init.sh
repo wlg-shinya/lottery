9a90dfa9257b188622a1e534771ebe6f64c9daf7
s-watanabe <s-watanabe@three-rings.net>
2024-06-08 14:16:52 +0900

    DBマイグレーション

--- initdb.d/init.sh
+++ initdb.d/init.sh
@@ -3,6 +3,3 @@ set -e
 dbname=lottery
 
 psql -c "CREATE DATABASE \"${dbname}\";"
-psql -d ${dbname} -c "CREATE TABLE lotteries();"
-psql -d ${dbname} -c "CREATE TABLE users();"
-psql -d ${dbname} -c "CREATE TABLE tokens();"
