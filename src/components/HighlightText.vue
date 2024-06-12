43db839b3da6a4c73d900840b6b8efc35200d731
s-watanabe <s-watanabe@three-rings.net>
2024-06-12 12:52:12 +0900

    サニタイズ部分の参照先を明記

--- src/components/HighlightText.vue
+++ src/components/HighlightText.vue
@@ -4,16 +4,28 @@ const props = defineProps<{
   highlight: string;
 }>();
 
-function sanitaize(s: string) {
-  return s.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#39;");
-}
+// ref. https://qiita.com/mas0061/items/c2e9cd0d27e09448d28e
+const sanitaize = {
+  encode: function (str: string) {
+    return str.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#39;");
+  },
+
+  decode: function (str: string) {
+    return str
+      .replace(/&lt;/g, "<")
+      .replace(/&gt;/g, ">")
+      .replace(/&quot;/g, '"')
+      .replace(/&#39;/g, "'")
+      .replace(/&amp;/g, "&");
+  },
+};
 
 function showText(): string {
   if (props.highlight) {
-    const textSan = sanitaize(props.text);
-    const highlightSan = sanitaize(props.highlight);
-    const regexp = new RegExp(highlightSan, "gi");
-    return textSan.replace(regexp, (x) => {
+    const safeText = sanitaize.encode(props.text);
+    const safeHighlight = sanitaize.encode(props.highlight);
+    const regexp = new RegExp(safeHighlight, "gi");
+    return safeText.replace(regexp, (x) => {
       return `<span class="highlight">${x}</span>`;
     });
   } else {
