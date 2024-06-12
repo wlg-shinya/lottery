664791cf4248e86f1576292faedb9649700712c3
symsystem <symsystem.a10@gmail.com>
2024-06-12 20:20:43 +0900

    email文字数制限追加

--- src/openapi/index.ts
+++ src/openapi/index.ts
@@ -14,6 +14,7 @@ export const VarcharMax = {
   lotteries_text: 256,
   lotteries_title: 32,
   lotteries_description: 256,
+  users_email: 80,
   users_account_name: 24,
   users_identification: 64,
   users_access_token: 64,
