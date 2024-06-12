9fb74ec8fe95687f87ea9a0a3c97d4486a6caae6
symsystem <symsystem.a10@gmail.com>
2024-06-12 19:51:54 +0900

    refactor

--- srv/api/crud/users.py
+++ srv/api/crud/users.py
@@ -11,7 +11,14 @@ import api.crud.tokens as tokens
 async def create_user(
     db: AsyncSession, body: schema.UserCreate
 ) -> Model:
-    model = Model(account_name=body.account_name, identification=hash(body.account_name, body.identification), pull_lottery_ids=[])
+    model = Model(
+        account_name=body.account_name, 
+        identification=hash(
+            account_name=body.account_name, 
+            identification=body.identification
+            ), 
+        pull_lottery_ids=[]
+        )
     return await _update_model(db=db, model=model)
 
 async def read_users(
@@ -23,7 +30,7 @@ async def read_users(
 async def read_user(
     db: AsyncSession, id: int
 ) -> Model:
-    model = await db.get(Model, id)
+    model = await db.get(entity=Model, ident=id)
     if model is None:
         raise HTTPException(status_code=404, detail=f"Not found id({id}) in {Model.__tablename__}")
     return model
@@ -31,14 +38,14 @@ async def read_user(
 async def read_user_by_access_token(
     db: AsyncSession, access_token: str
 ) -> Model:
-    tokens_model = await tokens.read_token(db, access_token)
-    return await read_user(db, tokens_model.user_id)
+    tokens_model = await tokens.read_token(db=db, access_token=access_token)
+    return await read_user(db=db, id=tokens_model.user_id)
 
 async def update_user(
     db: AsyncSession, body: schema.UserUpdate, original: Model
 ) -> Model:
     original.account_name = body.account_name
-    original.identification = hash(body.account_name, body.identification)
+    original.identification = hash(account_name=body.account_name, identification=body.identification)
     original.pull_lottery_ids = body.pull_lottery_ids
     return await _update_model(db=db, model=original)
 
@@ -60,20 +67,20 @@ async def signup(
     result = (await db.execute(select(Model).filter(Model.account_name == body.account_name)))
     if len(result.all()) > 0:
         raise HTTPException(status_code=400, detail=f"Bad Request already exists account_name({body.account_name})")
-    await create_user(db, body=schema.UserCreate(account_name=body.account_name, identification=body.identification))
+    await create_user(db, schema.UserCreate(account_name=body.account_name, identification=body.identification))
 
 async def signin(
     db: AsyncSession, body: schema.UserSignin
 ) -> schema.UserSigninResponse:
     # アカウントマッチング
-    access_token = hash(body.account_name, body.identification)
+    access_token = hash(account_name=body.account_name, identification=body.identification)
     row = (await db.execute(select(Model).filter(Model.account_name == body.account_name, Model.identification == access_token))).first()
     if row is None or len(row) == 0:
         raise HTTPException(status_code=404, detail="Not found account or password")
     users = row.tuple()[0]
     
     # トークン新規作成/更新
-    await _create_or_update_token(db, access_token, users.id)
+    await _create_or_update_token(db=db, access_token=access_token, id=users.id)
 
     return schema.UserSigninResponse(access_token=access_token)
 
@@ -81,24 +88,24 @@ async def change_password(
     db: AsyncSession, body: schema.UserChangePassword
 ) -> schema.UserChangePasswordResponse:
     # 現在のパスワードの検証
-    old_model = await read_user_by_access_token(db, body.access_token)
-    old_access_token = hash(old_model.account_name, body.old_password)
+    old_model = await read_user_by_access_token(db=db, access_token=body.access_token)
+    old_access_token = hash(account_name=old_model.account_name, identification=body.old_password)
     if old_access_token != old_model.identification:
         raise HTTPException(status_code=401, detail="Unauthorized")
     # 検証を通過したので新しいパスワードでユーザーを更新
     new_model = await update_user(
-        db, 
-        schema.UserUpdate(
+        db=db, 
+        body=schema.UserUpdate(
             account_name=old_model.account_name,
             identification=body.new_password,
             pull_lottery_ids=old_model.pull_lottery_ids,
             access_token=body.access_token
             ), 
-        old_model
+        original=old_model
         )
     new_access_token = new_model.identification
     # トークン新規作成/更新
-    await _create_or_update_token(db, new_access_token, new_model.id)
+    await _create_or_update_token(db=db, access_token=new_access_token, id=new_model.id)
     
     return schema.UserChangePasswordResponse(access_token=new_access_token)
 
@@ -114,10 +121,10 @@ async def _update_model(db: AsyncSession, model: Model) -> Model:
 
 async def _create_or_update_token(db: AsyncSession, access_token: str, id: int) -> None:
     tokens_body = TokenCreate(access_token=access_token, user_id=id)
-    tokens_model = await tokens.read_token(db, access_token)
+    tokens_model = await tokens.read_token(db=db, access_token=access_token)
     if tokens_model != None:
         # すでに存在しているならトークン有効期限を更新
-        await tokens.update_token(db, tokens_body, tokens_model)
+        await tokens.update_token(db=db, body=tokens_body, original=tokens_model)
     else:
         # 存在していないならトークン新規作成
-        await tokens.create_token(db, tokens_body)
+        await tokens.create_token(db=db, body=tokens_body)
