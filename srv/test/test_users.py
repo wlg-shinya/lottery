fc53d0bdc50d411745426f015993260458224a1b
s-watanabe <s-watanabe@three-rings.net>
2024-06-17 11:24:10 +0900

    refactor

--- srv/test/test_users.py
+++ srv/test/test_users.py
@@ -9,32 +9,32 @@ async def test_integration(client_generator):
     body_signup_step1 = schema.UserSignupStep1(email=body_signin.email, identification=body_signin.identification, account_name="test")
 
     # サインアップステップ1(Eメール非送信版)
-    res1 = await client.post(
+    res_signup_step1 = await client.post(
         "/api/admin/signup_step1",
         headers={"accept": "application/json"},
         content=body_signup_step1.model_dump_json()
         )
-    assert res1.status_code == 200
-    res1Obj = schema.UserSignupStep2(**res1.json())
-    assert res1Obj.signup_token != ""
+    assert res_signup_step1.status_code == 200
+    res_signup_step1_obj = schema.UserSignupStep2(**res_signup_step1.json())
+    assert res_signup_step1_obj.signup_token != ""
 
     # サインアップステップ2
-    res2 = await client.post(
+    res_signup_step2 = await client.post(
         "/api/signup_step2",
         headers={"accept": "application/json"},
-        content=res1.content
+        content=res_signup_step1.content
         )
-    assert res2.status_code == 200
+    assert res_signup_step2.status_code == 200
 
     # サインイン
-    res3 = await client.post(
+    res_signin = await client.post(
         "/api/signin",
         headers={"accept": "application/json"},
         content=body_signin.model_dump_json()
         )
-    assert res3.status_code == 200    
-    res3Obj = schema.UserSigninResponse(**res3.json())
-    assert res3Obj.access_token != ""
+    assert res_signin.status_code == 200    
+    res_signin_obj = schema.UserSigninResponse(**res_signin.json())
+    assert res_signin_obj.access_token != ""
 
     # 
-    access_token = res3Obj.access_token
+    access_token = res_signin_obj.access_token
