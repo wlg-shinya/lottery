fa09190aef4f8d70c0849c546a8d669e1cf90872
s-watanabe <s-watanabe@three-rings.net>
2024-06-17 12:03:03 +0900

    refactor

--- srv/test/test_users.py
+++ srv/test/test_users.py
@@ -1,23 +1,24 @@
 import pytest
 import api.schemas.users as schema
-from typing import List
-
+from httpx import Headers
+ 
 @pytest.mark.asyncio
 async def test_integration(client_generator):
     client = await client_generator.__anext__()
 
+    headers = Headers(headers={"accept": "application/json"}, encoding=None)
     body_signin = schema.UserSignin(email="test@dummy.com", identification="test")
     body_signup_step1 = schema.UserSignupStep1(email=body_signin.email, identification=body_signin.identification, account_name="test")
 
     # ユーザーは未登録なはず
-    read_users = await client.get("/api/read_users", headers={"accept": "application/json"})
+    read_users = await client.get("/api/read_users", headers=headers)
     assert read_users.status_code == 200
     assert len(read_users.json()) == 0
 
     # サインアップステップ1(Eメール非送信版)
     res_signup_step1 = await client.post(
         "/api/admin/signup_step1",
-        headers={"accept": "application/json"},
+        headers=headers,
         content=body_signup_step1.model_dump_json()
         )
     assert res_signup_step1.status_code == 200
@@ -27,20 +28,20 @@ async def test_integration(client_generator):
     # サインアップステップ2
     res_signup_step2 = await client.post(
         "/api/signup_step2",
-        headers={"accept": "application/json"},
+        headers=headers,
         content=res_signup_step1.content
         )
     assert res_signup_step2.status_code == 200
 
     # ユーザー登録数は1名になったはず
-    read_users = await client.get("/api/read_users", headers={"accept": "application/json"})
+    read_users = await client.get("/api/read_users", headers=headers)
     assert read_users.status_code == 200
     assert len(read_users.json()) == 1
 
     # サインイン
     res_signin = await client.post(
         "/api/signin",
-        headers={"accept": "application/json"},
+        headers=headers,
         content=body_signin.model_dump_json()
         )
     assert res_signin.status_code == 200
@@ -53,7 +54,7 @@ async def test_integration(client_generator):
     # アクセストークンでユーザー情報を得る
     res_read_user_by_access_token = await client.get(
         f"/api/read_user_by_access_token?access_token={access_token}", 
-        headers={"accept": "application/json"})
+        headers=headers)
     assert res_read_user_by_access_token.status_code == 200
     res_read_user_by_access_token_obj = schema.Users(**res_read_user_by_access_token.json())
     assert res_read_user_by_access_token_obj.email == body_signin.email
@@ -63,10 +64,9 @@ async def test_integration(client_generator):
     # IDでユーザー情報を得る
     res_read_user = await client.get(
         f"/api/read_user?id={res_read_user_by_access_token_obj.id}", 
-        headers={"accept": "application/json"})
+        headers=headers)
     assert res_read_user.status_code == 200
     res_read_user = schema.Users(**res_read_user.json())
     assert res_read_user.email == res_read_user_by_access_token_obj.email
     assert res_read_user.identification == res_read_user_by_access_token_obj.identification
     assert res_read_user.account_name == res_read_user_by_access_token_obj.account_name
-
